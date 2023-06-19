1cd4ef22fd89481c649ac583a4cbcb5cd1995494
MonikaPoradkova <136503636+MonikaPoradkova@users.noreply.github.com>
2023-06-19 22:47:38 +0200

    Projekt 2 - oprava - nový
    Dobrý den, vytvořila jsem úplně nový projekt - je to sice už pro projekt 3., ale snad nebude vadit.

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-const menuIcon = document.querySelector(".menu-icon")
-const menuList = document.querySelector("nav")
-const hamburgerIcon = document.querySelector(".fa-solid")
+const navMenu = document.querySelector(".nav-menu")
+const hamburger = document.querySelector(".hamburger")
 
-
-menuIcon.addEventListener("click", () => {
-    if (hamburgerIcon.classList[1] === "fa-bars") {
-        hamburgerIcon.classList.add("fa-xmark")
-        hamburgerIcon.classList.remove("fa-bars")
-        menuList.style.display = "block"  
-    } else {
-        hamburgerIcon.classList.add("fa-bars")
-        hamburgerIcon.classList.remove("fa-xmark")
-        menuList.style.display = "none"  
-    }
+hamburger.addEventListener("click", () =>{    
+    navMenu.classList.toggle("active");
+   hamburger.classList.toggle("active");
 })
